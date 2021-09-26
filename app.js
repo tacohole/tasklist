17fb4d9d6587de8061d9577003c83958b67f48d1
Troy Coll <troy.coll@gmail.com>
2021-09-26 07:32:59 -0400

    initial commit tasklist

---
+++ app.js
@@ -0,0 +1,64 @@
+// Define UI variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+    // add task event
+    form.addEventListener('submit', addTask);
+    //remove task event
+    taskList.addEventListener('click', removeTask);
+    // clear task event
+    clearBtn.addEventListener('click', clearAllTasks);
+
+}
+
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Add a Task');
+    }
+    // create li element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    // create text node and append
+
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // create new link element
+    const link = document.createElement('a');
+
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+
+    // clear input
+    taskInput.value = '';
+
+    e.preventDefault();
+}
+
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        console.log(e.target);
+        if (confirm('Sure?')) { e.target.parentElement.parentElement.remove() };
+    }
+
+    e.preventDefault();
+}
+
+function clearAllTasks() {
+    if (confirm('Sure?')) {
+        while (taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+    }
+}
