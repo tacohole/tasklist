e1a2da040c4f65de72171814fc7c42d30253a49e
Troy Coll <troy.coll@gmail.com>
2021-09-26 08:34:12 -0400

    filtering list

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ function loadEventListeners() {
     taskList.addEventListener('click', removeTask);
     // clear task event
     clearBtn.addEventListener('click', clearAllTasks);
+    // filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 
 }
 
@@ -61,4 +63,17 @@ function clearAllTasks() {
             taskList.removeChild(taskList.firstChild);
         }
     }
+}
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
